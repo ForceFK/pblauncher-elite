af78335b8592ff8e1b7cfe61c6330e223496a259
ForceFK <68686847+ForceFK@users.noreply.github.com>
2020-08-09 20:43:04 -0300

    Update Project

--- PBLauncher/PleaseWait.Designer.cs
+++ PBLauncher/PleaseWait.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace PBLauncher
 {
-    partial class Form1
+    partial class PleaseWait
     {
         /// <summary>
         /// Variável de designer necessária.
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // PleaseWait
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(366, 158);
+            this.Name = "PleaseWait";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- PBLauncher/Program.cs
+++ PBLauncher/Program.cs
@@ -16,7 +16,7 @@ namespace PBLauncher
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new PleaseWait());
         }
     }
 }
